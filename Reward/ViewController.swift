2bf50e9ec08be81c15df8130a1d8499ecd804687
Su Min Kim <kims622@gmail.com>
2018-11-10 14:16:48 -0500

    Initial Commit

---
+++ Reward/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reward
+//
+//  Created by Sumin Kim on 11/10/18.
+//  Copyright © 2018 Sumin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
