24093361486bf2d939d26212ae35d6248494459a
Sumin Kim <SuminKim@nbp-98-236.nbp.ruw.rutgers.edu>
2018-11-11 21:48:11 -0500

    first commit

---
+++ Reward/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Reward
+//
+//  Created by Sumin Kim on 11/10/18.
+//  Copyright © 2018 Sumin Kim. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Reward/SecondVC.swift
@@ -0,0 +1,9 @@
+//
+//  SecondVC.swift
+//  Reward
+//
+//  Created by Sumin Kim on 11/10/18.
+//  Copyright © 2018 Sumin Kim. All rights reserved.
+//
+
+import Foundation
